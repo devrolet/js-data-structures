6a9fe7756a9861464336131900657a6cf32b61eb
Chet Hill <chetdhill@gmail.com>
2025-04-09 20:48:42 -0500

    Prototype Object

--- User.js
+++ User.js
@@ -8,9 +8,17 @@ function User(firstName, lastName, age, gender) {
 }
 
 var user1 = new User('Gary', 'Johnson', '38', 'male');
-
 console.log(user1);
 
 var user200 = new User('Jamila', 'Harris', '32', 'female');
-
 console.log(user200);
+
+User.prototype.emailDomain = '@getremoted.com';
+
+console.log(user1);
+
+User.prototype.getEmailAddress = function() {
+    return this.firstName + this.lastName + this.emailDomain;
+}
+
+console.log(user1.getEmailAddress());
