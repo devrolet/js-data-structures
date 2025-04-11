9407f63afc6a109375b8becec65e675959369c21
Chet Hill <chetdhill@gmail.com>
2025-04-11 07:23:26 -0500

    Testing the addToHead and addToTail methods

--- LinkedList.js
+++ LinkedList.js
@@ -16,3 +16,22 @@ LinkedList.prototype.addToHead = function(value) {
     else this.tail = newNode;
     this.head = newNode;
 }
+
+LinkedList.prototype.addToTail = function(value) {
+    var newNode = new Node(value, null, this.tail);
+    if(this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
+}
+
+var myLL = new LinkedList();
+
+myLL.addToTail(10);
+myLL.addToTail(30);
+myLL.addToTail(50);
+
+myLL.addToHead(100);
+
+console.log(myLL.tail.prev);
+console.log(myLL.tail.prev.prev.prev);
+
