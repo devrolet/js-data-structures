3bd820b6c907b1287fcb00a3e7d02b90735a747a
Chet Hill <chetdhill@gmail.com>
2025-04-11 06:53:08 -0500

    Create Constructor functions for linked lists

---
+++ LinkedList.js
@@ -0,0 +1,15 @@
+function LinkedList(value, nextNode, prevNode) {
+    this.head = null;
+    this.tail = null;
+    
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+    var newNode = new Node(value, this.head, null);
+}
