b6c19adfa71b5f88acb968c86a56d7c57de84de1
Chet Hill <chetdhill@gmail.com>
2025-04-15 14:54:29 -0500

    Binary Search Tree: Insert Method with Recursion

--- Recursion.js
+++ Recursion.js
@@ -1,18 +1,12 @@
 // Recursion: When a function calls itself
 
-let func = () => {
-    if(/*base case*/) {
-        return something;
-    } else { // recursive case: calls the function
-        func();
-    }
-}
-
 const factorial = num => {
     if(num === 1) {
         return num;
     }else {
-        return num * factorial(num-1);
+        return num * factorial(num -1);
     }
 }
+
+factorial(4);
 
